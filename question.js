5f8371135c05324e83cd0943e5a90e4c4ef83bed
Altamash <ayank829130@gmail.com>
2024-08-01 14:36:44 +0530

    Initial Commit

---
+++ question.js
@@ -0,0 +1,45 @@
+// console.log("Hello World");
+// console.warn("Hello world");
+// alert("hello world");
+
+// ***********************
+// 2. JavaScript Program to Add Two Numbers
+// ***********************
+// function add(x = 0, y = 0) {
+//   return x + y;
+// }
+
+// let sum = (num1, num2) => {
+//   let total = num1 + num2;
+//   console.log(total);
+// };
+
+// function add(arr) {
+//   let total = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     total += arr[i];
+//   }
+//   console.log(total);
+// }
+// console.log(add([2, 3]));
+
+// ********************
+//3. JavaScript Program to Calculate the Area of a Triangle
+// ********************
+
+// function input(base, height) {
+//   return `Area of triangle is ${base * height * 0.5}`;
+// }
+
+// let info = (base, height) => {
+//   let areaOfTriangle = `${base * height * 0.5}`;
+//   console.log(`Area of triangle is ${areaOfTriangle}`);
+// };
+
+// ********************
+//4.JavaScript Program to Find the Square Root
+// ********************
+function squareRoot(num) {
+  let sqrt = Math.sqrt(num);
+  return `Square root of ${num} is ${sqrt}`;
+}
